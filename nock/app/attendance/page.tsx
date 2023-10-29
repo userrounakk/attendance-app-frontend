617d5e6c9a028791dd586d6cee51f4ce5ddbc869
lioncat2002 <sankhayan2002@gmail.com>
2023-10-29 18:31:13 +0530

    feat: added signin page

---
+++ nock/app/attendance/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+export default function AttendancePage() {
+  return (
+    <div>
+      <div className="p-8">
+        <div className="text-sm">Welcome 👋</div>
+        <div className="text-lg font-bold">Kitty</div>
+      </div>
+
+      <div className="w-full h-[1px] bg-slate-300 border-gray-600"></div>
+      <div className="flex flex-row justify-between p-8">
+        <ArrowBackIosIcon fontSize="small" color="primary" />
+        <div className="flex flex-row">
+          <CalendarTodayIcon fontSize="small" color="primary" />
+          <div className="px-1 text-sm text-blue-700">27 Oct 2023</div>
+        </div>
+
+        <ArrowForwardIosIcon fontSize="small" color="primary" />
+      </div>
+      <div className="flex flex-row justify-between rounded-lg p-4 my-6 mx-12 border-gray-400 border-2">
+        <div className="text-blue-700">Checkin Not enabled yet</div>
+        <CheckCircleIcon color="primary" />
+      </div>
+      <div className="py-12 flex flex-col items-center justify-center">
+        <img src="./Clock.svg" />
+        <div className="flex flex-row">
+          <div className="px-2 py-1">09:00 PM</div>
+          <img src="./Edit.svg" />
+        </div>
+        <div className="p-2 text-xs text-blue-700">Check-In Time</div>
+      </div>
+      <div className="py-4 flex flex-col items-center justify-center">
+        <img src="./Map.svg" />
+        <div className="flex flex-row">
+          <div className="p-2">Foodys, VIT</div>
+          <img src="./Edit.svg" />
+        </div>
+        <div className="p-2 text-xs text-blue-700">Location</div>
+      </div>
+      <div className="flex flex-row justify-between p-4 my-6 mx-12 border-blue-400 border-2">
+        <div className="text-blue-700">Enable Check In</div>
+        <CheckCircleIcon color="primary" />
+      </div>
+    </div>
+  );
+}
--- nock/app/page.tsx
+++ nock/app/page.tsx
@@ -1,49 +1,30 @@
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-
-export default function Home() {
+"use client";
+import { redirect, useRouter } from "next/navigation";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+export default function App() {
+  const { push } = useRouter();
   return (
-    <div>
-      <div className="p-8">
-        <div className="text-sm">Welcome 👋</div>
-        <div className="text-lg font-bold">Kitty</div>
+    <div className="h-full justify-between flex flex-col p-8">
+      <img src="SigninImg.svg" alt="" />
+      <div className="flex flex-row font-medium text-3xl">
+        Welcome to <div className="text-blue-700 px-2">Nock</div>
       </div>
-
-      <div className="w-full h-[1px] bg-slate-300 border-gray-600"></div>
-      <div className="flex flex-row justify-between p-8">
-        <ArrowBackIosIcon fontSize="small" color="primary" />
-        <div className="flex flex-row">
-          <CalendarTodayIcon fontSize="small" color="primary" />
-          <div className="px-1 text-sm text-blue-700">27 Oct 2023</div>
-        </div>
-
-        <ArrowForwardIosIcon fontSize="small" color="primary" />
+      <div className="text-sm font-medium">Smart Attendance Tracking App</div>
+      <div
+        onClick={() => {
+          console.log("miaow");
+          push("/attendance");
+        }}
+        className="m-12 p-8 text-center text-white font-medium bg-blue-700"
+      >
+        Sign In
+        <ArrowForwardIcon />
       </div>
-      <div className="flex flex-row justify-between rounded-lg p-4 my-6 mx-12 border-gray-400 border-2">
-        <div className="text-blue-700">Checkin Not enabled yet</div>
-        <CheckCircleIcon color="primary" />
-      </div>
-      <div className="py-12 flex flex-col items-center justify-center">
-        <img src="./Clock.svg" />
-        <div className="flex flex-row">
-          <div className="px-2 py-1">09:00 PM</div>
-          <img src="./Edit.svg" />
-        </div>
-        <div className="p-2 text-xs text-blue-700">Check-In Time</div>
-      </div>
-      <div className="py-4 flex flex-col items-center justify-center">
-        <img src="./Map.svg" />
-        <div className="flex flex-row">
-          <div className="p-2">Foodys, VIT</div>
-          <img src="./Edit.svg" />
-        </div>
-        <div className="p-2 text-xs text-blue-700">Location</div>
-      </div>
-      <div className="flex flex-row justify-between p-4 my-6 mx-12 border-blue-400 border-2">
-        <div className="text-blue-700">Enable Check In</div>
-        <CheckCircleIcon color="primary" />
+      <div className="text-xs flex flex-row justify-center">
+        Made with ❤️ By <div className="text-blue-700">G</div>
+        <div className="text-yellow-300">D</div>
+        <div className="text-red-500">S</div>
+        <div className="text-green-500">C</div>
       </div>
     </div>
   );
