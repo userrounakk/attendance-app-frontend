5eea939d22a17c0229dc7a987b0ac802ee82be68
lioncat2002 <sankhayan2002@gmail.com>
2023-11-14 22:21:18 +0530

    feat: added signup, signin and new team creation

--- nock/app/page.tsx
+++ nock/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { redirect, useRouter } from "next/navigation";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import Link from "next/link";
 export default function App() {
-  const { push } = useRouter();
   return (
     <div className="h-full justify-between flex flex-col p-8">
       <img src="SigninImg.svg" alt="" />
@@ -10,16 +9,12 @@ export default function App() {
         Welcome to <div className="text-blue-700 px-2">Nock</div>
       </div>
       <div className="text-sm font-medium">Smart Attendance Tracking App</div>
-      <div
-        onClick={() => {
-          console.log("miaow");
-          push("/attendance");
-        }}
-        className="m-12 p-8 text-center text-white font-medium bg-blue-700"
-      >
-        Sign In
-        <ArrowForwardIcon />
-      </div>
+      <Link href="/attendance">
+        <div className="m-12 p-8 text-center text-white font-medium bg-blue-700">
+          Sign In
+          <ArrowForwardIcon />
+        </div>
+      </Link>
       <div className="text-xs flex flex-row justify-center">
         Made with ❤️ By <div className="text-blue-700">G</div>
         <div className="text-yellow-300">D</div>
---
+++ nock/app/verify/page.tsx
@@ -0,0 +1,3 @@
+export default function Verify() {
+  return <div>Verified!</div>;
+}
