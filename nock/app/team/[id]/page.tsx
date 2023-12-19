4f855d817a427e2311108f01ca81c75402c9fe58
lioncat2002 <sankhayan2002@gmail.com>
2023-12-19 13:40:01 +0530

    feat: added get team member page

--- nock/app/team/[id]/page.tsx
+++ nock/app/team/[id]/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   params: {
@@ -7,6 +9,46 @@ type Props = {
 };
 
 export default function Team({ params }: Props) {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [teamJoinRequests, setTeamJoinRequests] = useState([]);
+  useEffect(() => {
+    async function getTeamMembers() {
+      const userData = localStorage.getItem("userData")!;
+      let data = JSON.parse(userData);
+      try {
+        const config = {
+          headers: { Authorization: `Bearer ${data.token}` },
+        };
+        let res = await axios.get(
+          `https://atapp.fly.dev/v1/team/${params.id}/members`,
+          config
+        );
+        setTeamMembers(res.data);
+        console.log(res.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getTeamMembers();
+    async function getTeamJoinRequests() {
+      const userData = localStorage.getItem("userData")!;
+      let data = JSON.parse(userData);
+      try {
+        const config = {
+          headers: { Authorization: `Bearer ${data.token}` },
+        };
+        let res = await axios.get(
+          `https://atapp.fly.dev/v1/team/${params.id}/requests`,
+          config
+        );
+        setTeamJoinRequests(res.data);
+        console.log(res.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getTeamJoinRequests();
+  }, []);
   console.log(params);
   return (
     <div className="px-4 py-8">
@@ -26,6 +68,30 @@ export default function Team({ params }: Props) {
           placeholder="Search for name,email or student id"
         />
       </label>
+      <div className="py-4 text-slate-500">Requests</div>
+      <div className="px-4">
+        {teamJoinRequests.map((member: any, idx: number) => {
+          return <div>{member.User.Name}</div>;
+        })}
+      </div>
+      <div className="py-4 text-slate-500">Admins</div>
+      <div className="px-4">
+        {teamMembers.map((member: any, idx: number) => {
+          if (member.Role === "super_admin")
+            return (
+              <div key={idx} className="flex flex-row justify-between">
+                <div>{member.User.Name}</div>
+                <div className="text-slate-500">Super Admin</div>
+              </div>
+            );
+        })}
+      </div>
+      <div className="py-4 text-slate-500">Participant</div>
+      <div className="px-4">
+        {teamMembers.map((member: any, idx: number) => {
+          if (member.Role === "member") return <div>{member.User.Name}</div>;
+        })}
+      </div>
     </div>
   );
 }
